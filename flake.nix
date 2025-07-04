53cc7b40586eacf2d266c2fe9fedd4800bc91fa4
Vladimr Markov <withoutboat@gmail.com>
2025-07-04 11:46:21 -0500

    feat: init

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Flake-based Neovim config for Home Manager";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = _: {
+    homeManagerModules.default = _: {
+      programs.neovim = {
+        enable = true;
+      };
+    };
+  };
+}
