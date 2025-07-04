c3e03e48ae5f160e2bbce15ed2cadb7559874911
Vladimr Markov <withoutboat@gmail.com>
2025-07-04 17:31:41 -0500

    feat: telescope

--- telescope/telescope.lua
+++ telescope/telescope.lua
@@ -1,7 +1,5 @@
-local status, telescope = pcall(require, "telescope")
-if not status then
-	return
-end
+local telescope = require("telescope")
+
 local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
 
@@ -49,6 +47,7 @@ telescope.setup({
 		},
 
 		n = {
+			["q"] = actions.close,
 			["<esc>"] = actions.close,
 			["<CR>"] = actions.select_default,
 			["<C-x>"] = actions.select_horizontal,
@@ -80,39 +79,57 @@ telescope.setup({
 			["?"] = actions.which_key,
 		},
 	},
+
 	defaults = {
 		mappings = {
 			n = {
 				["q"] = actions.close,
 			},
 		},
+		file_ignore_patterns = {
+			"node_modules",
+			".git/",
+			"target/",
+			"dist/",
+			"__pycache__/",
+		},
 	},
 	extensions = {
-		file_browser = {
-			theme = "dropdown",
-			-- disables netrw and use telescope-file-browser in its place
-			hijack_netrw = true,
-			mappings = {
-				-- your custom insert mode mappings
-				["i"] = {
-					["<C-w>"] = function()
-						vim.cmd("normal vbd")
-					end,
-				},
-				["n"] = {
-					-- your custom normal mode mappings
-					["N"] = fb_actions.create,
-					["h"] = fb_actions.goto_parent_dir,
-					["/"] = function()
-						vim.cmd("startinsert")
-					end,
-				},
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true,
+			override_file_sorter = true,
+			case_mode = "smart_case",
+		},
+		file_browser = {},
+		--	projects = {},
+	},
+	file_browser = {
+		theme = "dropdown",
+		-- disables netrw and use telescope-file-browser in its place
+		hijack_netrw = true,
+		mappings = {
+			-- your custom insert mode mappings
+			["i"] = {
+				["<C-w>"] = function()
+					vim.cmd("normal vbd")
+				end,
+			},
+			["n"] = {
+				-- your custom normal mode mappings
+				["N"] = fb_actions.create,
+				["h"] = fb_actions.goto_parent_dir,
+				["/"] = function()
+					vim.cmd("startinsert")
+				end,
 			},
 		},
 	},
 })
 
+telescope.load_extension("fzf")
 telescope.load_extension("file_browser")
+--telescope.load_extension("projects")
 
 vim.keymap.set("n", ";f", function()
 	builtin.find_files({
@@ -120,21 +137,27 @@ vim.keymap.set("n", ";f", function()
 		hidden = true,
 	})
 end)
+
 vim.keymap.set("n", ";r", function()
 	builtin.live_grep()
 end)
+
 vim.keymap.set("n", "\\\\", function()
 	builtin.buffers()
 end)
+
 vim.keymap.set("n", ";t", function()
 	builtin.help_tags()
 end)
+
 vim.keymap.set("n", ";;", function()
 	builtin.resume()
 end)
+
 vim.keymap.set("n", ";e", function()
 	builtin.diagnostics()
 end)
+
 vim.keymap.set("n", "sf", function()
 	telescope.extensions.file_browser.file_browser({
 		path = "%:p:h",
