ef176a848e78ac02b3bd225c1aa52ccef5eca527
Vladimr Markov <withoutboat@gmail.com>
2025-07-06 11:13:46 -0500

    feat: copilot

--- lsp/default.nix
+++ lsp/default.nix
@@ -13,26 +13,8 @@
     cmp_luasnip
     luasnip
     lspkind-nvim
-
-    (pkgs.vimUtils.buildVimPlugin {
-      name = "copilot-lua";
-      src = pkgs.fetchFromGitHub {
-        owner = "zbirenbaum";
-        repo = "copilot.lua";
-        rev = "46f4b7d026cba9497159dcd3e6aa61a185cb1c48";
-        sha256 = "sha256-p4kZBoa0GMV1s4gNfQ3i2ZTVN8RNxOi3EJ7pH6nw6qk";
-      };
-    })
-
-    (pkgs.vimUtils.buildVimPlugin {
-      name = "copilot-cmp";
-      src = pkgs.fetchFromGitHub {
-        owner = "zbirenbaum";
-        repo = "copilot-cmp";
-        rev = "15fc12af3d0109fa76b60b5cffa1373697e261d1";
-        sha256 = "sha256-erRL8bY/zuwuCZfttw+avTrFV7pjv2H6v73NzY2bymM=";
-      };
-    })
+    copilot-cmp
+	copilot-lua
   ];
 
   extraPackages = with pkgs; [
