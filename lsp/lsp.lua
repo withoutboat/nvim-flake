fb0c2d206d7d507d5a0f4e2d3e333a08c67b62f2
Vladimr Markov <withoutboat@gmail.com>
2025-07-06 06:25:19 -0500

    feat: conform

--- lsp/lsp.lua
+++ lsp/lsp.lua
@@ -1,4 +1,20 @@
 local lspconfig = require("lspconfig")
 
+local diagnostic_config = {
+	signs = {
+		active = true,
+		values = {
+			{ name = "DiagnosticSignError", text = "" },
+			{ name = "DiagnosticSignWarn", text = "" },
+			{ name = "DiagnosticSignHint", text = "" },
+			{ name = "DiagnosticSignInfo", text = "" },
+		},
+	},
+	virtual_text = true,
+	update_in_insert = false, -- Keep signs visible in insert mode
+}
+
+vim.diagnostic.config(diagnostic_config)
+
 lspconfig.lua_ls.setup({})
 lspconfig.nixd.setup({})
