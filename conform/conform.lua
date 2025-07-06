1fcf83664a03614468b05c69b0419fceb2b6b739
Vladimr Markov <withoutboat@gmail.com>
2025-07-06 05:17:37 -0500

    feat: conform

--- conform/conform.lua
+++ conform/conform.lua
@@ -1,11 +1,8 @@
-return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      nix = { "alejandra" },
-      lua = { "stylua" },
-    },
-    format_on_save = { lsp_fallback = true },
-    notify_on_error = true,
+require("conform").setup({
+  formatters_by_ft = {
+    nix = { "alejandra" },
+    lua = { "stylua" },
   },
-}
+  format_on_save = { lsp_fallback = true },
+  notify_on_error = true,
+})
