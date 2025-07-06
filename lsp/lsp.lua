5d8c168d8777aed40e76bcac82372e665627a474
Vladimr Markov <withoutboat@gmail.com>
2025-07-06 11:18:14 -0500

    feat: copilot

--- lsp/lsp.lua
+++ lsp/lsp.lua
@@ -81,9 +81,12 @@ local luasnip = require("luasnip")
 
 local select_opts = { behavior = cmp.SelectBehavior.Select }
 local lspkind = require("lspkind")
-require("copilot_cmp").setup({
-	method = "getCompletionsCycling", -- or use "getCompletions" if preferred
+require("copilot").setup({
+  suggestion = { enabled = false },  
+  panel = { enabled = false },
 })
+
+require("copilot_cmp").setup()
 
 cmp.setup({
 	snippet = {
