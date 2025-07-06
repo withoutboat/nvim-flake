2e50a092c25b8e675a6f2c6621d46e2b98f47286
Vladimr Markov <withoutboat@gmail.com>
2025-07-06 01:00:48 -0500

    feat: stylix

--- flake.nix
+++ flake.nix
@@ -5,33 +5,34 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = {...}: {
-    homeManagerModules.default = let
- staticConfig = ''
-    luafile ${builtins.toString ./lua/base.lua}
-  '';
+  outputs = { self, nixpkgs, ... }: {
+    homeManagerModules.default = { pkgs, lib, ... }: 
+      let
+        staticConfig = ''
+          luafile ${builtins.toString ./lua/base.lua}
+        '';
+        telescope = import ./telescope/default.nix { inherit pkgs; };
+      in {
+        programs.neovim = {
+          enable = true;
+          viAlias = true;
+          defaultEditor = true;
 
-  telescope = import ./telescope/default.nix { inherit pkgs; };
-    in {
-      pkgs,
-      lib, ... }:  {
-      programs.neovim = {
-        enable = true;
-        viAlias = true;
-	defaultEditor = true;
+          plugins = telescope.plugins
+            ++ (with pkgs.vimPlugins; [
+              # Add additional plugins here if needed
+            ]);
 
-        plugins = telescope.plugins
-        ++ (with pkgs.vimPlugins; []);
+          extraPackages = telescope.extraPackages
+            ++ (with pkgs; [
+              # Add additional tools here if needed
+            ]);
 
-        extraPackages = with pkgs; [
-            ripgrep
-    		fd 
-        ];
-
-	extraConfig = builtins.concatStringsSep "\n" (
-      [ staticConfig ] 
-    );
+          extraConfig = builtins.concatStringsSep "\n" [
+            staticConfig
+            # Add additional config strings here if needed
+          ];
+        };
       };
-    };
   };
 }
