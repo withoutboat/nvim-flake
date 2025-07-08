598c5e08147c75c954df5027e3a7d400a0356c98
Vladimr Markov <withoutboat@gmail.com>
2025-07-08 00:53:58 -0500

    feat: copilot

--- flake.nix
+++ flake.nix
@@ -5,36 +5,44 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    homeManagerModules.default = { pkgs, lib, ... }: 
-      let
-        staticConfig = ''
-          luafile ${builtins.toString ./lua/base.lua}
-        '';
-        telescope = import ./telescope { inherit pkgs; };
-        threesitter = import ./threesitter { inherit pkgs; };
-        lsp = import ./lsp { inherit pkgs; };
-        conform = import ./conform { inherit pkgs; };
-      in {
-        programs.neovim = {
-          enable = true;
-          viAlias = true;
-          defaultEditor = true;
+  outputs = {...}: {
+    homeManagerModules.default = {pkgs, ...}: let
+      staticConfig = ''
+        luafile ${builtins.toString ./lua/base.lua}
+      '';
+      telescope = import ./telescope {inherit pkgs;};
+      threesitter = import ./threesitter {inherit pkgs;};
+      lsp = import ./lsp {inherit pkgs;};
+      conform = import ./conform {inherit pkgs;};
+      copilot = import ./copilot {inherit pkgs;};
+    in {
+      programs.neovim = {
+        enable = true;
+        viAlias = true;
+        defaultEditor = true;
 
-          plugins = telescope.plugins ++ threesitter.plugins ++ lsp.plugins ++ conform.plugins
-            ++ (with pkgs.vimPlugins; [
+        plugins =
+          telescope.plugins
+          ++ threesitter.plugins
+          ++ lsp.plugins
+          ++ conform.plugins
+          ++ copilot.plugins
+          ++ (with pkgs.vimPlugins; [
             ]);
 
-            extraPackages = lsp.extraPackages ++ conform.extraPackages ++ (with pkgs; [
-              ripgrep
-              fd
-            ]);
+        extraPackages =
+          lsp.extraPackages
+          ++ conform.extraPackages
+          ++ (with pkgs; [
+            ripgrep
+            fd
+          ]);
 
-          extraConfig = builtins.concatStringsSep "\n" [
-            staticConfig
-            # Add additional config strings here if needed
-          ];
-        };
+        extraConfig = builtins.concatStringsSep "\n" [
+          staticConfig
+          # Add additional config strings here if needed
+        ];
       };
+    };
   };
 }
